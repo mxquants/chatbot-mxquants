71a2b0c969d3473ddf98d26cb55451b7bc8414be
rhdzmota <rohdzmota@gmail.com>
2017-03-26 16:52:00 -0600

    s

--- respond.py
+++ respond.py
@@ -14,10 +14,10 @@ def verify():
     # the 'hub.challenge' value it receives in the query arguments
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
         if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
-            return "Verification token mismatch", 403
-        return request.args["hub.challenge"], 200
+            return "Verification token mismatch"#, 403
+        return request.args["hub.challenge"]#, 200
 
-    return "Hello world", 200
+    return "Hello world"#, 200
 
 
 @app.route('/', methods=['POST'])
